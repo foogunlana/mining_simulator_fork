d601cb31fb50e320e8c5055eeebac9d8c61db186
Foluso Ogunlana <folusoogunlana@Folusos-MacBook-Air.local>
2018-06-18 18:22:41 +0100

    minor change

--- src/mining_game/miner_result.cpp
+++ src/mining_game/miner_result.cpp
@@ -6,9 +6,11 @@
 #include "miner_result.hpp"
 #include "block.hpp"
 
+#include <iostream>
+
 namespace mining_game {
 
-    MinerResult::MinerResult() : blocksInWinningChain(BlockCount(0)), totalProfit(Value(0)) {}
+    MinerResult::MinerResult() : blocksInWinningChain(BlockCount(0)), totalProfit(Profit(0)) {}
 
     void MinerResult::addBlock(const Block *block) {
         totalProfit += block->realValue();
